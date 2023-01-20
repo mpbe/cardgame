a44fc65b0d0e6670168ddfeb271dc79b1e6d91a4
jonny <fuckoff@msn.com>
2023-01-20 16:50:30 +0000

    Hello mike

--- src/com/mike24/Main.java
+++ src/com/mike24/Main.java
@@ -3,7 +3,7 @@ package com.mike24;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
+	// write your code heresout
+        System.out.println("hello, mike");
     }
 }
