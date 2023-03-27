56e02c70cca198639814b6d17f84d14d0dfed1d3
jonny <fuckoff@msn.com>
2023-03-27 09:54:29 +0100

    code updated

--- src/com/mike24/Cards.java
+++ src/com/mike24/Cards.java
@@ -1,47 +1,124 @@
 package com.mike24;
-
-
-/* i have no idea what im doing lol. do i have to make 52 objects????
-  still going through treehouse videos but havent seen loads on methods yet
-  so not sure how to approach making shuffle etc. guess i need to make the cards
-  first tho. is this correct in any way or am i way off??
-
- */
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Cards {
 
-    final private String cardName;
-    final private String cardSuit;
-    final private int cardValue;
+    final public int value;
+    Suits suit;
 
-    public Cards(String cardName, String cardSuit, int cardValue) {
-        this.cardName = cardName;
-        this.cardSuit = cardSuit;
-        this.cardValue = cardValue;
+    public Cards(int value, Suits suit) {
+        this.value = value;
+        this.suit = suit;
+
+
     }
 
-    public String getCardName(){
+    //not sure what i would use a getter for here? is there any use for it?
+    public int getValue() {
 
-        return cardName;
+        return value;
     }
 
-    public String getCardSuit(){
-
-        return cardSuit;
+    @Override
+    public String toString() {
+        return value + " OF " + suit;
     }
 
-    public int getCardValue(){
+    //create a list of cards using the i to populate - push and pop in lists/collection
+    ArrayList<Cards> listOfCards = new ArrayList<Cards>();
 
-        return cardValue;
+    public ArrayList<Cards> cardBuilder() {
+
+        for (Suits suit : Suits.values()) {
+            /* ive done an enhanced for loop here but how would i write this as a
+            regular for loop? i ask cos the syntax of enums kinda confuses mee
+             */
+            for (int i = 0; i < 10; i++) {
+                Cards cards = new Cards(i + 2, suit);
+
+                listOfCards.add(cards);
+                if (i == 8) {
+                    for (int j = 0; j < 3; j++) {
+                        listOfCards.add(cards);
+                        /*ive done this because i couldnt think of another way to make
+                        // the face cards have the same value of 10
+                         ace is done in the final i loop. */
+
+
+
+                        /* a problem i have though is i want to assign names to the cards
+                         (and then use that in the toString method instead of int value)
+                         but not sure the best way to do this? was thinking of making a
+                         separate enum for names but then it gets messy with how to assign
+                         them and suit together. as i said i find enum syntax confusing
+
+                         */
+
+                    }
+                }
+            }
+
+        }
+
+
+        return (listOfCards);
     }
 
-    public String cardCheck()
-    {
-        return("this card is "+ this.getCardName()+
-                " of suit " +
-                this.getCardSuit()+", its value is " + this.getCardValue());
+    int total = 0;
+
+    int deal() {
+        System.out.println("your cards are:");
+
+
+        for (int i = 0; i < 2; i++) {
+
+            System.out.println(listOfCards.get(i));
+            total += listOfCards.get(i).value;
+
+
+        }
+        return total;
     }
 
 
+    boolean isPlaying = true;
+    Scanner scanner = new Scanner(System.in);
+    int hitCounter = 2;
+
+    public int play() {
+
+
+        do {
+
+            if (total > 21) {
+                System.out.println("busted!");
+                break;
+                //can just exit the program altogether here how do i do that?
+            }
+
+            System.out.println("What is your move?");
+            System.out.println("HIT    STAND");
+            String move = scanner.nextLine();
+
+            if (move.equalsIgnoreCase("hit")) {
+
+                System.out.println(listOfCards.get(hitCounter));
+                total += listOfCards.get(hitCounter).value;
+                hitCounter++;
+                System.out.printf("total is %d\n", total);
+
+            } else if (move.equalsIgnoreCase("stand")) {
+                System.out.println("standing");
+                System.out.printf("your total was %d", total);
+                isPlaying = false;
+            } else {
+                System.out.println("please enter a valid move");
+            }
+
+        } while(isPlaying);
+
+        return total;
+    }
 }
--- src/com/mike24/Main.java
+++ src/com/mike24/Main.java
@@ -1,41 +1,36 @@
 package com.mike24;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-        // object that represents a card. It will hold the information of the individual card
-
-        // store these ina  collection - 52 cards, suits etc
-
-        // what actions does deck of cards need to do (methods draw card eg)
-        // how to show what cards you are dealth before you start dealing
-
-        Cards sixDiamond = new Cards("6D", "diamonds", 6);{
-            System.out.println(sixDiamond.cardCheck());
-        }
-
-        Cards twoClubs = new Cards("2C", "clubs", 2);{
-            System.out.println(twoClubs.cardCheck());
-        }
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("what is your username");
         String userName = scanner.nextLine();
-        System.out.printf("hi %s, welcome to scuffedjack. i will eventually come up with a way to deal you some cards", userName);
+        System.out.printf("hi %s, welcome to scuffedjack\n", userName);
 
 
+        Cards cards = new Cards(1, Suits.CLUBS);
 
+        ArrayList<Cards> listOfCards = cards.cardBuilder();
 
+        Collections.shuffle(listOfCards);
+        //System.out.println(listOfCards);
 
+        int total = cards.deal();
+
+        total = cards.play();
 
 
 
 
     }
 }
+
 
